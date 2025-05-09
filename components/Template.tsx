9569be4874f885e4b0ed206b54ece1c0896a4ffa
ardofulian <careday17@gmail.com>
2025-05-09 23:44:07 +0700

    add preview

--- components/Template.tsx
+++ components/Template.tsx
@@ -39,7 +39,7 @@ export default function Template(
         <meta name="title" content={`Kemal Ardian | ${pageTitle}`} />
         <meta name="description" content={pageDescription} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="theme-color" content={previewConfig.color} />
+        <meta name="theme-color" data-react-helmet="true" content={previewConfig.color} />
 
         {/* Open Graph / Facebook */}
         <meta property="og:type" content="website" />
